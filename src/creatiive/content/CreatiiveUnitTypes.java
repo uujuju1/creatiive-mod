bd3315ec14042b5914c20a33e6be66d0a766d531
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-01 19:13:43 -0300

    Add files via upload

--- src/creatiive/content/CreatiiveUnitTypes.java
+++ src/creatiive/content/CreatiiveUnitTypes.java
@@ -2,19 +2,35 @@ package creatiive.content;
 
 import creatiive.type.*;
 
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.struct.*;
 import arc.graphics.Color;
-import mindustry.content.*;
+import mindustry.ai.types.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.ctype.*;
 import mindustry.ctype.ContentList;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
-import mindustry.entities.effect.MultiEffect;
+import mindustry.entities.effect.*;
 import mindustry.gen.*;
-import mindustry.graphics.Pal;
+import mindustry.graphics.*;
 import mindustry.type.*;
 import mindustry.type.weapons.*;
+import mindustry.world.meta.*;
+import mindustry.entities.effect.MultiEffect;
+import mindustry.graphics.Pal;
 import mindustry.type.ammo.ItemAmmoType;
 import mindustry.type.weapons.PointDefenseWeapon;
 import mindustry.world.meta.BlockFlag;
 
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
 import static mindustry.Vars.tilesize;
 import static mindustry.content.Bullets.*;
 
@@ -329,7 +345,7 @@ public class CreatiiveUnitTypes implements ContentList {
             );
 		}};
 
-		gold = new HydronavalUnitType("gold") {{
+		gold = new RotorUnitType("gold") {{
 			localizedName = "Gold";
 			canBoost = false;
 			flying = false;
