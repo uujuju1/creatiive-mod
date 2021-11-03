bcb06050ed50a010d973ecfe6e5a817108b4212b
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-03 11:37:59 -0300

    Add files via upload

--- src/creatiive/entities/bullet/WaterMissileBulletType.java
+++ src/creatiive/entities/bullet/WaterMissileBulletType.java
@@ -17,23 +17,14 @@ import static mindustry.Vars.tilesize;
 public class WaterMissileBulletType extends BulletType {
 	public float groundDrag = 0.15f;
 	public float waterDrag = 0.015f;
-	public float deepDrag = -0.015f;
 
 	public WaterMissileBulletType(float speed, float damage) {
 		super(speed, damage);
 		layer = Layer.scorch;
         homingPower = 0.035f;
         homingRange = 20 * tilesize;
-        hitShake = 24;
-        knockback = 8f;
-        hitSize = 16f;
-        collideTerrain = collideFloor = true;
         keepVelocity = collidesAir = absorbable = hittable = reflectable = false;
-        lightColor = hitColor;
         trailRotation = true;
-        hitEffect = new MultiEffect(Fx.smokeCloud, Fx.blastExplosion, UAWFxStatic.torpedoRippleHit, UAWFxDynamic.smokeCloud(trailColor));
-        status = StatusEffects.slow;
-        statusDuration = 3 * 60;
         hitSoundVolume = 4f;
         hitSound = Sounds.explosionbig;
 	}
@@ -41,13 +32,10 @@ public class WaterMissileBulletType extends BulletType {
 	@Override
 	public void update(Bullet b) {
 		Floor floor = Vars.world.floorWorld(b.x, b.y);
-		if (!floor.isLiquid) {
-			b.vel.scl(Math.max(1f - groundDrag * Time.delta, 0.01f));
-		} else {
+		if (floor.isLiquid) {
 			b.vel.scl(Math.max(1f - waterDrag * Time.delta, 0.01f));
-		}
-		if (floor.isDeep) {
-			b.vel.scl(Math.max(1f - deepDrag * Time.delta, 0.01f));
+		} else {
+			b.vel.scl(Math.max(1f - groundDrag * Time.delta, 0.01f));
 		}
 		super.update(b);
 	}
