e00edd0e2d61aff1e78ff3dd4692b34288dcaf61
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-14 21:33:26 -0300

    Add files via upload

--- src/creatiive/world/blocks/ScatterBlock.java
+++ src/creatiive/world/blocks/ScatterBlock.java
@@ -26,24 +26,22 @@ import mindustry.type.*;
 import mindustry.world.draw.*;
 
 public class ScatterBlock extends Block {
-	public BulletType bullet = Bullets.standardCopper;
-	public Color heatColor = Pal.turretHeat;
-	public Effect shootEffect = Fx.none;
+    public BulletType bullet = Bullets.standardCopper;
+    public Color heatColor = Pal.turretHeat;
+    public Effect shootEffect = Fx.none;
     public Effect smokeEffect = Fx.none;
     public Effect ammoUseEffect = Fx.none;
     public Sound shootSound = Sounds.shoot;
     public Item ammo = Items.copper;
     public TextureRegion baseRegion, turretRegion, heatRegion;
-
+    
     public int shots = 10;
 
-    float alpha = 0f;
-
     public ScatterBlock(String name) {
-    	super(name);
-    	destructible = true;
-    	hasItems = true;
-    	update = true;
+        super(name);
+        destructible = true;
+        hasItems = true;
+        update = true;
         solid = true;
         outlineIcon = true;
         group = BlockGroup.turrets;
@@ -53,37 +51,28 @@ public class ScatterBlock extends Block {
     }
 
     @Override
-    public void load() {
-    	baseRegion = Core.atlas.find(name + "-base");
-    	turretRegion = Core.atlas.find(name);
-    	heatRegion = Core.atlas.find(name + "-heat");
+    public load() {
+        baseRegion = Core.atlas.find(name + "-base");
+        turretRegion = Core.atlas.find(name);
+        heatRegion = Core.atlas.find(name + "-heat");
     }
 
-   	public class ScatterBlockBuild extends Building { 
-   		@Override
-   		public void updateTile() {
-   			// check consume for shoot
-   			if (cons.valid()) {
-   				for (int i = 0; i <= shots; i++) {
-   					bullet.create(this, this.team, x, y, Mathf.random() * 360f);
-   				}
-   				consume();
-   				alpha+=0.5f;
-   			}
+    public class ScatterBlockBuild extends Building { 
+        @Override
+        public void updateTile() {
+            // check consume for shoot
+            if (cons.valid()) {
+                for (int i = 0; i <= shots; i++) {
+                    bullet.create(this, this.team, x, y, Mathf.random() * 360f);
+                }
+            consume();
+            }
+        }
 
-   			// heat
-   			if (alpha != 0f) {
-   				alpha-=0.001f;
-   			}
-   		}
-
-   		@Override 
-   		public void draw() {
-   			Draw.rect(baseRegion, x, y, 0);
-   			Draw.rect(turretRegion, x, y, 0);
-   			Draw.alpha(alpha);
-   			Draw.color(heatColor);
-   			Draw.rect(heatRegion, x, y, 0);
-   		}
-   	}
+        @Override 
+        public void draw() {
+            Draw.rect(baseRegion, x, y, 0);
+            Draw.rect(turretRegion, x, y, 0);
+        }
+    }
 }
