13ac4e16b1085b00b81ffef096149679c12940cf
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-20 11:28:32 -0300

    Add files via upload

--- src/creatiive/type/ShieldUnitType.java
+++ src/creatiive/type/ShieldUnitType.java
@@ -21,7 +21,6 @@ public class ShieldUnitType extends UnitType {
     // public TextureRegion shieldRegion;
 
 	public int reloadTime = 5;
-	float rel = 0;
 	// float alpha = 0;
 
 	public ShieldUnitType(String name) {
@@ -34,15 +33,16 @@ public class ShieldUnitType extends UnitType {
 	public void update(Unit unit) {
 		float x = unit.vel.x;
 		float y = unit.vel.y;
+		float rel;
 		if (Math.abs(x) >= 2.5f || Math.abs(y) >= 2.5f) {
-			// if (rel <= 0) {
-				shootType.create(unit, unit.team, unit.x, unit.y, unit.rotation + (Mathf.random(-1, 1) * 45));
-				// rel = 5;
-			// }
+			if (rel <= 0) {
+				shootType.create(unit, unit.team, unit.x, unit.y, unit.rotation - 22.5f + (Mathf.random() * 45f));
+				rel = reloadTime;
+			}
 		}
-		// if (rel >= 0f) {
-			// rel--;
-		// }
+		if (rel >= 0f) {
+			rel--;
+		}
 	}
 
 	/**
