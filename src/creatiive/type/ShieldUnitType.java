f3fb95136d669e931c0014a92a9a8400ddaf70f1
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-11-20 16:40:58 -0300

    Update ShieldUnitType.java

--- src/creatiive/type/ShieldUnitType.java
+++ src/creatiive/type/ShieldUnitType.java
@@ -17,11 +17,7 @@ import mindustry.entities.bullet.*;
 public class ShieldUnitType extends UnitType {
 	public BulletType shootType = Bullets.standardCopper;
 
-    public Color shieldColor = Pal.lancerLaser;
-    public TextureRegion shieldRegion;
-
 	public int reloadTime = 5;
-	// float alpha = 0;
 
 	public ShieldUnitType(String name) {
 		super(name);
@@ -36,18 +32,5 @@ public class ShieldUnitType extends UnitType {
 		if (Math.abs(x) >= 2.5f || Math.abs(y) >= 2.5f) {
 				shootType.create(unit, unit.team, unit.x, unit.y, unit.rotation - 22.5f + (Mathf.random() * 45f));
 		}
-	}
-
-	@Override
-	public void load() {
-		shieldRegion = Core.atlas.find(name + "-shield");
-	}
-
-	@Override
-	public void draw(Unit unit) {
-		super.draw(unit);
-		Draw.color(shieldColor);
-		Draw.rect(shieldRegion, unit.x, unit.y, unit.rotation);
-		Draw.color();
 	}
 }
