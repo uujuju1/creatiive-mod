3ea07cbdff74849d97bfc531272a96e1935bfaea
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-01 13:17:55 -0300

    Add files via upload

--- src/creatiive/graphics/CreatiiveFx.java
+++ src/creatiive/graphics/CreatiiveFx.java
@@ -32,9 +32,9 @@ public class CreatiiveFx {
         Drawf.tri(e.x, e.y, w, 4f * e.fout(), e.rotation + 180f);
 	}),
 	dynamicPlastShards = new DynamicEffect(40f, 3, e -> {
-		randLenVectors(e.id, scl * 2, 10 + e.fin() * 5, (x, y) -> {
+		randLenVectors(e.id, e.intensity * 2f, 10 + e.fin() * 5, (x, y) -> {
 			color(Color.valueOf("92C966"), Color.valueOf("73BD36"), e.fin());
-			Fill.rect(e.x + x, e.y + y, e.fout() * scl, e.fout() * scl, 45);
+			Fill.rect(e.x + x, e.y + y, e.fout() * e.intensity, e.fout() * e.intensity, 45);
 		});
 	});
 }
