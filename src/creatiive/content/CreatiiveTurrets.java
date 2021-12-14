396feb11f7a1a613e7d60031de6995e0e758bf05
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-13 21:02:20 -0300

    Add files via upload

--- src/creatiive/content/CreatiiveTurrets.java
+++ src/creatiive/content/CreatiiveTurrets.java
@@ -354,7 +354,7 @@ public class CreatiiveTurrets implements ContentList {
 			ammo(
 				CreatiiveItems.lerbonium, new BasicBulletType(4f, 1) {{
 					fragBullets = 1;
-					fragBullet = new DelayBulletType(0f, 100) {{hitSize = 12; lifetime 120}};
+					fragBullet = new DelayBulletType(0f, 100) {{hitSize = 12; lifetime = 120;}};
 				}}
 			);
 		}};
