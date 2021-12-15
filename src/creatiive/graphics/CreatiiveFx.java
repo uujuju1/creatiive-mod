a99777232b0bee6beb94e9e495896670c3b0ea8d
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-15 11:22:04 -0300

    Add files via upload

--- src/creatiive/graphics/CreatiiveFx.java
+++ src/creatiive/graphics/CreatiiveFx.java
@@ -63,7 +63,7 @@ public class CreatiiveFx {
 		color(Color.valueOf("BA5A69"));
 
 		randLenVectors(e.id + 1, 10, 20f + e.fin() * 20f, (x, y) -> {
-			Fill.rect(e.x + x, e.y + y, Math.sin(e.fin() * Math.PI) * 10f, Math.sin(e.fin() * Math.PI) * 10f, 45f);
+			Fill.rect(e.x + x, e.y + y, Mathf.sin(e.fin() * Math.PI) * 10f, Mathf.sin(e.fin() * Math.PI) * 10f, 45f);
 		});
 	});
 }
