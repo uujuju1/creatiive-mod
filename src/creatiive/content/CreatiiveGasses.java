41a104cdf76f8d4eb4cb084f5284eea56f383f5d
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-16 13:21:26 -0300

    Add files via upload

--- src/creatiive/content/CreatiiveGasses.java
+++ src/creatiive/content/CreatiiveGasses.java
@@ -5,18 +5,11 @@ import gas.type.Gas;
 import mindustry.ctype.ContentList;
 
 public class CreatiiveGasses implements ContentList {
-	public static Gas steam, fissileFuel;
-
+	public static Gas steam;
 	@Override
 	public void load() {
 		steam = new Gas("steam") {{
-			localizedName = "Steam";
 			color = Color.valueOf("C3C3C3");
-		}};
-		fissileFuel = new Gas("fissile-fuel") {{
-			localizedName = "Fissile Fuel";
-			color = Color.valueOf("565E59");
-			radioactivity = 0.5f;
 		}};
 	}
 }
