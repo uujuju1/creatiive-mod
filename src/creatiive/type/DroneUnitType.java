6f9708f07384d5b4957830c44ae95fcd9fe18d9c
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-18 20:15:59 -0300

    Add files via upload

--- src/creatiive/type/DroneUnitType.java
+++ src/creatiive/type/DroneUnitType.java
@@ -6,6 +6,7 @@ import mindustry.type.*;
 public class DroneUnitType extends UnitType {
 	public int flameDamage = 1;
 	public DroneUnitType(String name) {
+		super(name);
 		flying = lowAltitude = true;
 		constructor = UnitEntity::create;
 	}
